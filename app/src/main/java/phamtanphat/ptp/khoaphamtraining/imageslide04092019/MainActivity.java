432bbbfe5d9ffcb3cee9f13a45d8b62f8942ed53
phatdroid94 <phatdroid94@gmail.com>
2019-09-30 21:14:37 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/imageslide04092019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/imageslide04092019/MainActivity.java
@@ -5,28 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView mimg;
+    Button mbtnSlide;
+    int[] mArrayanimal = {R.drawable.ic_cat,
+                          R.drawable.ic_dog,
+                          R.drawable.ic_fish,
+                          R.drawable.ic_lion,
+                          R.drawable.ic_monkey};
+    int index = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        event();
+    }
 
-        //Countdowntimer
-        CountDownTimer countDownTimer = new CountDownTimer(10000,500) {
+    private void event() {
+        mbtnSlide.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTick(long l) {
-                Log.d("BBB", Math.round( l / 1000f) + "");
+            public void onClick(View view) {
+                CountDownTimer countDownTimer = new CountDownTimer(6000, 1000) {
+                    @Override
+                    public void onTick(long l) {
+                       mimg.setImageResource(mArrayanimal[index++]);
+                    }
+
+                    @Override
+                    public void onFinish() {
+
+                    }
+                };
+                countDownTimer.start();
             }
+        });
+    }
 
-            @Override
-            public void onFinish() {
-
-            }
-        };
-        countDownTimer.start();
-
-
+    private void init() {
+        mimg = findViewById(R.id.imaeview);
+        mbtnSlide = findViewById(R.id.buttonSlideImage);
     }
 }
